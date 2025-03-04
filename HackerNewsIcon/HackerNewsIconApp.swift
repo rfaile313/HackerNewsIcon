e1cbbdcac0d224455927a5552c7889af788750ea
Rudy Faile <rudy_faile@redlands.edu>
2025-03-04 14:28:30 -0600

    Initial Commit

---
+++ HackerNewsIcon/HackerNewsIconApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsIconApp.swift
+//  HackerNewsIcon
+//
+//  Created by Rudy Faile on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsIconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
